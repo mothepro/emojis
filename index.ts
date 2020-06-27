d41aeedbc7a37ea8be0d038d71688c26732944f7
Maurice Prosper <mothepro@google.com>
2020-06-27 15:04:55 -0500

    use typescript cause im not wanting to build d.ts files manually

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
-import * as emojis from './emojis.json'
+import allEmojis from './emojis.json'
 
-export default emojis
+export default allEmojis
 
 /** Yields an emoji and its description in random order. */
 export function* randomPairs() {
-  const emojis = Object.keys(emojiMap)
+  const emojis = Object.keys(allEmojis) as Array<keyof typeof allEmojis>
   for (let i = emojis.length - 1; i > 0; i--) {
     const j = Math.trunc(Math.random() * i);
     [emojis[j], emojis[i]] = [emojis[i], emojis[j]]
   }
 
   for (const emoji of emojis)
-    yield { emoji, description: emojiMap[emoji] }
+    yield { emoji, description: allEmojis[emoji] }
 }
 
 /** Yields all emojis and their descriptions as a string in random order. */
@@ -22,9 +22,8 @@ export function* randomStrings(prefix = false) {
       : description + ' ' + emoji
 }
 
-
 /** Continuously yields emojis and their descriptions as a string. */
-export function* foreverStrings(prefix = false) {
+export function* foreverStrings(prefix = false): Generator<string, never, void> {
   while (true)
     yield* randomStrings(prefix)
 }
